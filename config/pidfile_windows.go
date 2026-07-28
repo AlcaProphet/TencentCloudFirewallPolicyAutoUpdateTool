c80ea2895c48155e9434678390f4013222689c82
AlcaProphet <evanchenyf123@hotmail.com>
2026-07-28 10:38:42 +0800

    chore(workflow): 更新 Node.js 版本并修复 Windows 进程检查常量
    - 将 GitHub Actions 工作流中的 Node.js 版本从 20 更新到 24
    - 添加 stillActiveExitCode 常量定义用于 Windows 进程状态检查
    - 修复 processExists 函数中对 STILL_ACTIVE 状态码的引用方式

--- config/pidfile_windows.go
+++ config/pidfile_windows.go
@@ -6,6 +6,9 @@ import (
 	"golang.org/x/sys/windows"
 )
 
+// stillActiveExitCode 是 GetExitCodeProcess 对仍在运行的进程返回的状态码
+const stillActiveExitCode = 259
+
 func processExists(pid int) bool {
 	h, err := windows.OpenProcess(windows.PROCESS_QUERY_LIMITED_INFORMATION, false, uint32(pid))
 	if err != nil {
@@ -14,5 +17,5 @@ func processExists(pid int) bool {
 	defer windows.CloseHandle(h)
 	var code uint32
 	err = windows.GetExitCodeProcess(h, &code)
-	return err == nil && code == windows.STILL_ACTIVE
+	return err == nil && code == uint32(stillActiveExitCode)
 }
